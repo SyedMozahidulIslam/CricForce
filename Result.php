7fe0d173993bbfb77545f697fbc128dca7e68f06
Syed Mozahidul Islam Fahim <116684492+SyedMozahidulIslam@users.noreply.github.com>
2023-11-26 11:07:51 +0600

    Add files via upload

--- Result.php
+++ Result.php
@@ -2,13 +2,30 @@
 
 declare(strict_types=1);
 
-namespace Doctrine\DBAL\Driver;
+namespace Doctrine\DBAL;
 
-/**
- * Driver-level statement execution result.
- */
-interface Result
+use Doctrine\DBAL\Driver\Exception as DriverException;
+use Doctrine\DBAL\Driver\Result as DriverResult;
+use Doctrine\DBAL\Exception\NoKeyValue;
+use Doctrine\Deprecations\Deprecation;
+use LogicException;
+use Traversable;
+
+use function array_shift;
+use function func_num_args;
+
+class Result
 {
+    private DriverResult $result;
+    private Connection $connection;
+
+    /** @internal The result can be only instantiated by {@see Connection} or {@see Statement}. */
+    public function __construct(DriverResult $result, Connection $connection)
+    {
+        $this->result     = $result;
+        $this->connection = $connection;
+    }
+
     /**
      * Returns the next row of the result as a numeric array or FALSE if there are no more rows.
      *
@@ -16,7 +33,14 @@ interface Result
      *
      * @throws Exception
      */
-    public function fetchNumeric();
+    public function fetchNumeric()
+    {
+        try {
+            return $this->result->fetchNumeric();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
 
     /**
      * Returns the next row of the result as an associative array or FALSE if there are no more rows.
@@ -25,7 +49,14 @@ interface Result
      *
      * @throws Exception
      */
-    public function fetchAssociative();
+    public function fetchAssociative()
+    {
+        try {
+            return $this->result->fetchAssociative();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
 
     /**
      * Returns the first value of the next row of the result or FALSE if there are no more rows.
@@ -34,7 +65,14 @@ interface Result
      *
      * @throws Exception
      */
-    public function fetchOne();
+    public function fetchOne()
+    {
+        try {
+            return $this->result->fetchOne();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
 
     /**
      * Returns an array containing all of the result rows represented as numeric arrays.
@@ -43,7 +81,14 @@ interface Result
      *
      * @throws Exception
      */
-    public function fetchAllNumeric(): array;
+    public function fetchAllNumeric(): array
+    {
+        try {
+            return $this->result->fetchAllNumeric();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
 
     /**
      * Returns an array containing all of the result rows represented as associative arrays.
@@ -52,42 +97,243 @@ interface Result
      *
      * @throws Exception
      */
-    public function fetchAllAssociative(): array;
+    public function fetchAllAssociative(): array
+    {
+        try {
+            return $this->result->fetchAllAssociative();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
 
     /**
      * Returns an array containing the values of the first column of the result.
+     *
+     * @return array<mixed,mixed>
+     *
+     * @throws Exception
+     */
+    public function fetchAllKeyValue(): array
+    {
+        $this->ensureHasKeyValue();
+
+        $data = [];
+
+        foreach ($this->fetchAllNumeric() as [$key, $value]) {
+            $data[$key] = $value;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Returns an associative array with the keys mapped to the first column and the values being
+     * an associative array representing the rest of the columns and their values.
+     *
+     * @return array<mixed,array<string,mixed>>
+     *
+     * @throws Exception
+     */
+    public function fetchAllAssociativeIndexed(): array
+    {
+        $data = [];
+
+        foreach ($this->fetchAllAssociative() as $row) {
+            $data[array_shift($row)] = $row;
+        }
+
+        return $data;
+    }
+
+    /**
+     * @return list<mixed>
+     *
+     * @throws Exception
+     */
+    public function fetchFirstColumn(): array
+    {
+        try {
+            return $this->result->fetchFirstColumn();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
+
+    /**
+     * @return Traversable<int,list<mixed>>
+     *
+     * @throws Exception
+     */
+    public function iterateNumeric(): Traversable
+    {
+        while (($row = $this->fetchNumeric()) !== false) {
+            yield $row;
+        }
+    }
+
+    /**
+     * @return Traversable<int,array<string,mixed>>
+     *
+     * @throws Exception
+     */
+    public function iterateAssociative(): Traversable
+    {
+        while (($row = $this->fetchAssociative()) !== false) {
+            yield $row;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws Exception
+     */
+    public function iterateKeyValue(): Traversable
+    {
+        $this->ensureHasKeyValue();
+
+        foreach ($this->iterateNumeric() as [$key, $value]) {
+            yield $key => $value;
+        }
+    }
+
+    /**
+     * Returns an iterator over the result set with the keys mapped to the first column and the values being
+     * an associative array representing the rest of the columns and their values.
+     *
+     * @return Traversable<mixed,array<string,mixed>>
+     *
+     * @throws Exception
+     */
+    public function iterateAssociativeIndexed(): Traversable
+    {
+        foreach ($this->iterateAssociative() as $row) {
+            yield array_shift($row) => $row;
+        }
+    }
+
+    /**
+     * @return Traversable<int,mixed>
+     *
+     * @throws Exception
+     */
+    public function iterateColumn(): Traversable
+    {
+        while (($value = $this->fetchOne()) !== false) {
+            yield $value;
+        }
+    }
+
+    /** @throws Exception */
+    public function rowCount(): int
+    {
+        try {
+            return $this->result->rowCount();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
+
+    /** @throws Exception */
+    public function columnCount(): int
+    {
+        try {
+            return $this->result->columnCount();
+        } catch (DriverException $e) {
+            throw $this->connection->convertException($e);
+        }
+    }
+
+    public function free(): void
+    {
+        $this->result->free();
+    }
+
+    /** @throws Exception */
+    private function ensureHasKeyValue(): void
+    {
+        $columnCount = $this->columnCount();
+
+        if ($columnCount < 2) {
+            throw NoKeyValue::fromColumnCount($columnCount);
+        }
+    }
+
+    /**
+     * BC layer for a wide-spread use-case of old DBAL APIs
+     *
+     * @deprecated Use {@see fetchNumeric()}, {@see fetchAssociative()} or {@see fetchOne()} instead.
+     *
+     * @psalm-param FetchMode::* $mode
+     *
+     * @return mixed
+     *
+     * @throws Exception
+     */
+    public function fetch(int $mode = FetchMode::ASSOCIATIVE)
+    {
+        Deprecation::trigger(
+            'doctrine/dbal',
+            'https://github.com/doctrine/dbal/pull/4007',
+            '%s is deprecated, please use fetchNumeric(), fetchAssociative() or fetchOne() instead.',
+            __METHOD__,
+        );
+
+        if (func_num_args() > 1) {
+            throw new LogicException('Only invocations with one argument are still supported by this legacy API.');
+        }
+
+        if ($mode === FetchMode::ASSOCIATIVE) {
+            return $this->fetchAssociative();
+        }
+
+        if ($mode === FetchMode::NUMERIC) {
+            return $this->fetchNumeric();
+        }
+
+        if ($mode === FetchMode::COLUMN) {
+            return $this->fetchOne();
+        }
+
+        throw new LogicException('Only fetch modes declared on Doctrine\DBAL\FetchMode are supported by legacy API.');
+    }
+
+    /**
+     * BC layer for a wide-spread use-case of old DBAL APIs
+     *
+     * @deprecated Use {@see fetchAllNumeric()}, {@see fetchAllAssociative()} or {@see fetchFirstColumn()} instead.
+     *
+     * @psalm-param FetchMode::* $mode
      *
      * @return list<mixed>
      *
      * @throws Exception
      */
-    public function fetchFirstColumn(): array;
+    public function fetchAll(int $mode = FetchMode::ASSOCIATIVE): array
+    {
+        Deprecation::trigger(
+            'doctrine/dbal',
+            'https://github.com/doctrine/dbal/pull/4007',
+            '%s is deprecated, please use fetchAllNumeric(), fetchAllAssociative() or fetchFirstColumn() instead.',
+            __METHOD__,
+        );
 
-    /**
-     * Returns the number of rows affected by the DELETE, INSERT, or UPDATE statement that produced the result.
-     *
-     * If the statement executed a SELECT query or a similar platform-specific SQL (e.g. DESCRIBE, SHOW, etc.),
-     * some database drivers may return the number of rows returned by that query. However, this behaviour
-     * is not guaranteed for all drivers and should not be relied on in portable applications.
-     *
-     * @return int The number of rows.
-     *
-     * @throws Exception
-     */
-    public function rowCount(): int;
+        if (func_num_args() > 1) {
+            throw new LogicException('Only invocations with one argument are still supported by this legacy API.');
+        }
 
-    /**
-     * Returns the number of columns in the result
-     *
-     * @return int The number of columns in the result. If the columns cannot be counted,
-     *             this method must return 0.
-     *
-     * @throws Exception
-     */
-    public function columnCount(): int;
+        if ($mode === FetchMode::ASSOCIATIVE) {
+            return $this->fetchAllAssociative();
+        }
 
-    /**
-     * Discards the non-fetched portion of the result, enabling the originating statement to be executed again.
-     */
-    public function free(): void;
+        if ($mode === FetchMode::NUMERIC) {
+            return $this->fetchAllNumeric();
+        }
+
+        if ($mode === FetchMode::COLUMN) {
+            return $this->fetchFirstColumn();
+        }
+
+        throw new LogicException('Only fetch modes declared on Doctrine\DBAL\FetchMode are supported by legacy API.');
+    }
 }
--- Statement.php
+++ Statement.php
@@ -1,78 +1,261 @@
 <?php
 
-namespace Doctrine\DBAL\Driver;
+namespace Doctrine\DBAL;
 
-use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Deprecations\Deprecation;
+
+use function func_num_args;
+use function is_string;
 
 /**
- * Driver-level statement
+ * A database abstraction-level statement that implements support for logging, DBAL mapping types, etc.
  */
-interface Statement
+class Statement
 {
     /**
-     * Binds a value to a corresponding named (not supported by mysqli driver, see comment below) or positional
-     * placeholder in the SQL statement that was used to prepare the statement.
+     * The SQL statement.
      *
-     * As mentioned above, the named parameters are not natively supported by the mysqli driver, use executeQuery(),
-     * fetchAll(), fetchArray(), fetchColumn(), fetchAssoc() methods to have the named parameter emulated by doctrine.
+     * @var string
+     */
+    protected $sql;
+
+    /**
+     * The bound parameters.
      *
-     * @param string|int $param Parameter identifier. For a prepared statement using named placeholders,
-     *                          this will be a parameter name of the form :name. For a prepared statement
-     *                          using question mark placeholders, this will be the 1-indexed position of the parameter.
-     * @param mixed      $value The value to bind to the parameter.
-     * @param int        $type  Explicit data type for the parameter using the {@see ParameterType}
-     *                          constants.
+     * @var mixed[]
+     */
+    protected $params = [];
+
+    /**
+     * The parameter types.
      *
-     * @return bool TRUE on success or FALSE on failure.
+     * @var int[]|string[]
+     */
+    protected $types = [];
+
+    /**
+     * The underlying driver statement.
+     *
+     * @var Driver\Statement
+     */
+    protected $stmt;
+
+    /**
+     * The underlying database platform.
+     *
+     * @var AbstractPlatform
+     */
+    protected $platform;
+
+    /**
+     * The connection this statement is bound to and executed on.
+     *
+     * @var Connection
+     */
+    protected $conn;
+
+    /**
+     * Creates a new <tt>Statement</tt> for the given SQL and <tt>Connection</tt>.
+     *
+     * @internal The statement can be only instantiated by {@see Connection}.
+     *
+     * @param Connection       $conn      The connection for handling statement errors.
+     * @param Driver\Statement $statement The underlying driver-level statement.
+     * @param string           $sql       The SQL of the statement.
      *
      * @throws Exception
      */
-    public function bindValue($param, $value, $type = ParameterType::STRING);
+    public function __construct(Connection $conn, Driver\Statement $statement, string $sql)
+    {
+        $this->conn     = $conn;
+        $this->stmt     = $statement;
+        $this->sql      = $sql;
+        $this->platform = $conn->getDatabasePlatform();
+    }
 
     /**
-     * Binds a PHP variable to a corresponding named (not supported by mysqli driver, see comment below) or question
-     * mark placeholder in the SQL statement that was use to prepare the statement. Unlike {@see bindValue()},
-     * the variable is bound as a reference and will only be evaluated at the time
-     * that PDOStatement->execute() is called.
+     * Binds a parameter value to the statement.
      *
-     * As mentioned above, the named parameters are not natively supported by the mysqli driver, use executeQuery(),
-     * fetchAll(), fetchArray(), fetchColumn(), fetchAssoc() methods to have the named parameter emulated by doctrine.
+     * The value can optionally be bound with a DBAL mapping type.
+     * If bound with a DBAL mapping type, the binding type is derived from the mapping
+     * type and the value undergoes the conversion routines of the mapping type before
+     * being bound.
      *
-     * Most parameters are input parameters, that is, parameters that are
-     * used in a read-only fashion to build up the query. Some drivers support the invocation
-     * of stored procedures that return data as output parameters, and some also as input/output
-     * parameters that both send in data and are updated to receive it.
+     * @param string|int $param The name or position of the parameter.
+     * @param mixed      $value The value of the parameter.
+     * @param mixed      $type  Either a PDO binding type or a DBAL mapping type name or instance.
+     *
+     * @return bool TRUE on success, FALSE on failure.
+     *
+     * @throws Exception
+     */
+    public function bindValue($param, $value, $type = ParameterType::STRING)
+    {
+        $this->params[$param] = $value;
+        $this->types[$param]  = $type;
+
+        $bindingType = ParameterType::STRING;
+
+        if ($type !== null) {
+            if (is_string($type)) {
+                $type = Type::getType($type);
+            }
+
+            $bindingType = $type;
+
+            if ($type instanceof Type) {
+                $value       = $type->convertToDatabaseValue($value, $this->platform);
+                $bindingType = $type->getBindingType();
+            }
+        }
+
+        try {
+            return $this->stmt->bindValue($param, $value, $bindingType);
+        } catch (Driver\Exception $e) {
+            throw $this->conn->convertException($e);
+        }
+    }
+
+    /**
+     * Binds a parameter to a value by reference.
+     *
+     * Binding a parameter by reference does not support DBAL mapping types.
      *
      * @deprecated Use {@see bindValue()} instead.
      *
-     * @param string|int $param    Parameter identifier. For a prepared statement using named placeholders,
-     *                             this will be a parameter name of the form :name. For a prepared statement using
-     *                             question mark placeholders, this will be the 1-indexed position of the parameter.
-     * @param mixed      $variable Name of the PHP variable to bind to the SQL statement parameter.
-     * @param int        $type     Explicit data type for the parameter using the {@see ParameterType}
-     *                             constants.
-     * @param int|null   $length   You must specify maxlength when using an OUT bind
+     * @param string|int $param    The name or position of the parameter.
+     * @param mixed      $variable The reference to the variable to bind.
+     * @param int        $type     The binding type.
+     * @param int|null   $length   Must be specified when using an OUT bind
      *                             so that PHP allocates enough memory to hold the returned value.
      *
-     * @return bool TRUE on success or FALSE on failure.
+     * @return bool TRUE on success, FALSE on failure.
      *
      * @throws Exception
      */
-    public function bindParam($param, &$variable, $type = ParameterType::STRING, $length = null);
+    public function bindParam($param, &$variable, $type = ParameterType::STRING, $length = null)
+    {
+        Deprecation::trigger(
+            'doctrine/dbal',
+            'https://github.com/doctrine/dbal/pull/5563',
+            '%s is deprecated. Use bindValue() instead.',
+            __METHOD__,
+        );
+
+        $this->params[$param] = $variable;
+        $this->types[$param]  = $type;
+
+        try {
+            if (func_num_args() > 3) {
+                return $this->stmt->bindParam($param, $variable, $type, $length);
+            }
+
+            return $this->stmt->bindParam($param, $variable, $type);
+        } catch (Driver\Exception $e) {
+            throw $this->conn->convertException($e);
+        }
+    }
 
     /**
-     * Executes a prepared statement
+     * Executes the statement with the currently bound parameters.
      *
-     * If the prepared statement included parameter markers, you must either:
-     * call {@see bindParam()} to bind PHP variables to the parameter markers:
-     * bound variables pass their value as input and receive the output value,
-     * if any, of their associated parameter markers or pass an array of input-only
-     * parameter values.
+     * @deprecated Statement::execute() is deprecated, use Statement::executeQuery() or executeStatement() instead
      *
-     * @param mixed[]|null $params A numeric array of values with as many elements as there are
-     *                             bound parameters in the SQL statement being executed.
+     * @param mixed[]|null $params
      *
      * @throws Exception
      */
-    public function execute($params = null): Result;
+    public function execute($params = null): Result
+    {
+        Deprecation::triggerIfCalledFromOutside(
+            'doctrine/dbal',
+            'https://github.com/doctrine/dbal/pull/4580',
+            '%s() is deprecated, use Statement::executeQuery() or Statement::executeStatement() instead',
+            __METHOD__,
+        );
+
+        if ($params !== null) {
+            $this->params = $params;
+        }
+
+        $logger = $this->conn->getConfiguration()->getSQLLogger();
+        if ($logger !== null) {
+            $logger->startQuery($this->sql, $this->params, $this->types);
+        }
+
+        try {
+            return new Result(
+                $this->stmt->execute($params),
+                $this->conn,
+            );
+        } catch (Driver\Exception $ex) {
+            throw $this->conn->convertExceptionDuringQuery($ex, $this->sql, $this->params, $this->types);
+        } finally {
+            if ($logger !== null) {
+                $logger->stopQuery();
+            }
+        }
+    }
+
+    /**
+     * Executes the statement with the currently bound parameters and return result.
+     *
+     * @param mixed[] $params
+     *
+     * @throws Exception
+     */
+    public function executeQuery(array $params = []): Result
+    {
+        if (func_num_args() > 0) {
+            Deprecation::trigger(
+                'doctrine/dbal',
+                'https://github.com/doctrine/dbal/pull/5556',
+                'Passing $params to Statement::executeQuery() is deprecated. Bind parameters using'
+                    . ' Statement::bindParam() or Statement::bindValue() instead.',
+            );
+        }
+
+        if ($params === []) {
+            $params = null; // Workaround as long execute() exists and used internally.
+        }
+
+        return $this->execute($params);
+    }
+
+    /**
+     * Executes the statement with the currently bound parameters and return affected rows.
+     *
+     * @param mixed[] $params
+     *
+     * @throws Exception
+     */
+    public function executeStatement(array $params = []): int
+    {
+        if (func_num_args() > 0) {
+            Deprecation::trigger(
+                'doctrine/dbal',
+                'https://github.com/doctrine/dbal/pull/5556',
+                'Passing $params to Statement::executeStatement() is deprecated. Bind parameters using'
+                    . ' Statement::bindParam() or Statement::bindValue() instead.',
+            );
+        }
+
+        if ($params === []) {
+            $params = null; // Workaround as long execute() exists and used internally.
+        }
+
+        return $this->execute($params)->rowCount();
+    }
+
+    /**
+     * Gets the wrapped driver statement.
+     *
+     * @return Driver\Statement
+     */
+    public function getWrappedStatement()
+    {
+        return $this->stmt;
+    }
 }
